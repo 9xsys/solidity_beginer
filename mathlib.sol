64e835c0ea318c2ce6d1514d1af319b3d3818623
9xsys <v.monteiro.web@gmail.com>
2025-05-09 11:18:01 +0400

    upload mathlib contract

---
+++ mathlib.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract mathLib{
+    function add(uint a, uint b) public pure returns(uint){
+        return a + b;
+    }
+
+    function mul(uint a, uint b) public pure returns(uint){
+        return a * b;
+    }
+
+    
+}
