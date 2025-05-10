314789e59ec2af1ef90afd4c77b40c793ee45e4e
9xsys <v.monteiro.web@gmail.com>
2025-05-10 22:33:36 +0400

    upload firstToken contract

---
+++ first_erc_20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract firstToken is ERC20 {
+    constructor() ERC20("9Xsys", "NNX"){
+        _mint(msg.sender, 100 * 10 **18);
+    } 
+}
+
